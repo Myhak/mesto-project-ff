47bf658da3d92bdeba056fcd8426a914fd625ff9
Maksim Nikonenko <myhak356@yandex.ru>
2025-02-11 14:04:33 +0300

    1 версия

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,43 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template');
+
+const placesList = document.querySelector('.places__list');
+
+function removeCard(cardElement) {
+  cardElement.remove();
+}
+
+function createCard(cardData) {
+  const cardElement = cardTemplate.content.cloneNode(true).querySelector('.places__item');
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    removeCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+function renderInitialCards() {
+  if (initialCards) {
+    initialCards.forEach((cardData) => {
+      const card = createCard(cardData);
+
+      placesList.append(card);
+    });
+  } else {
+    console.error('Массив initialCards не найден!');
+  }
+}
+
+renderInitialCards();
